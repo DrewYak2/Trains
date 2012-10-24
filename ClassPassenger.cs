eb8037ce22546be76ae552f1db5c246c6af42581
Andrew <DrewYak7@google.com>
2012-10-24 12:08:34 +0400

    First Save

---
+++ ClassPassenger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+class DemoPassengers
+{        
+    [Test]
+    public static UInt32 Summa(UInt32 a, UInt32 b)
+    {
+        return a + b;
+    }
+}
+
+class DemoTest
+{    
+    [Test]
+    public static void MyTestFunction()
+    {
+        UInt32 a = 2;
+        UInt32 b = 5;
+
+        Assert.IsTrue(DemoPassengers.Summa(a, b) >= 0);
+        Assert.IsInstanceOf<String>(DemoPassengers.Summa(a, b));
+        Assert.AreEqual(a + b, DemoPassengers.Summa(a, b));
+    }
+
+    [Test]
+    public static void MyTestFunction_2()
+    {
+        UInt32 a = 2;
+        UInt32 b = 5;
+
+        Assert.IsTrue(DemoPassengers.Summa(a, b) >= 0);
+        Assert.IsInstanceOf<UInt32>(DemoPassengers.Summa(a, b));
+        Assert.AreEqual(a + b, DemoPassengers.Summa(a, b));
+    }
+
+}
